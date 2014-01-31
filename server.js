4bb766c8705985b6ac39b6401af0854f69cf1c61
Wanasit Tanakitrungruang <berrysboy@gmail.com>
2014-01-31 17:02:05 +0900

    Init commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+
+app.listen(process.env.PORT || 8082);
+
+function handler (req, res) {
+  res.writeHead(200);
+  res.end("This is a Socket IO backend for a real time location sharing service.\n");
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.on('location', function (data) {
+    io.sockets.emit('location', data);
+  });
+});
